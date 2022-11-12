6fe48e9a8433fb653c35d961b3f50119f7a7f52f
anton.hritsan@gameloft.com <anton.hritsan@gameloft.com>
2022-11-12 13:29:17 +0200

    + added controller and player prefabs + added code for move player by controller

---
+++ Assets/Controller.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Controller : MonoBehaviour
+{
+	CircleCollider2D CircleCollider2D;
+	GameObject Inner;
+
+	Vector2 Direction = Vector2.zero;
+	float Angle = 0.0f;
+
+	bool isCaptured = false;
+	Vector2 CapturePos;
+	// Start is called before the first frame update
+	void Start()
+	{
+		this.CircleCollider2D = GetComponent<CircleCollider2D>();
+		this.Inner = this.transform.Find("Inner").gameObject;
+
+		this.SetVisible(true);
+	}
+
+	public void SetVisible(bool value)
+	{
+		this.GetComponent<Renderer>().enabled = value;
+		this.Inner.GetComponent<Renderer>().enabled = value;
+	}
+
+	public Vector2 getDirection()
+	{
+		return this.Direction;
+	}
+	public void SetPosition(Vector2 pos)
+	{
+		this.transform.position = pos;
+	}
+
+	public float getAngle()
+	{
+		return this.Angle;
+	}
+
+	public void BeginTouch(Vector2 mousePos2D)
+	{
+		RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+		if (hit.collider != null)
+		{
+			this.isCaptured = true;
+			this.CapturePos = mousePos2D;
+			this.Inner.transform.localPosition = new Vector3(0.0f, 0.0f);
+		}
+	}
+
+	public void MoveTouch(Vector2 mousePos2D)
+	{
+		if (!this.isCaptured)
+			return;
+
+		RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+		if (hit.collider == null)
+		{
+			this.EndTouch();
+			return;
+		}
+
+		Vector2 Diff = (mousePos2D - this.CapturePos) / this.transform.localScale.x;
+
+		// bound to distance
+		Vector2 newPos = this.Inner.transform.localPosition + new Vector3(Diff.x, Diff.y);
+		float Radius = newPos.magnitude;
+		Radius = Math.Clamp(Radius, 0.0f, 0.4f);// hardcode!
+
+		this.Direction = newPos.normalized;
+		this.Inner.transform.localPosition = this.Direction * Radius;
+		this.CapturePos = mousePos2D;
+
+		this.Angle = Mathf.Atan2(this.Inner.transform.localPosition.y, this.Inner.transform.localPosition.x) * Mathf.Rad2Deg;
+		
+	}
+
+	public void EndTouch()
+	{
+		this.Direction = Vector2.zero;
+		if (!this.isCaptured)
+			return;
+
+		//return back inner
+		// release capture
+		this.isCaptured = false;
+		this.Inner.transform.localPosition = new Vector3(0.0f, 0.0f);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.touchCount != 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			Debug.Log(touch.azimuthAngle);
+		}
+
+		//this.UpdateInner();
+
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float fCoefMove = 0.001f;
+    SpriteRenderer spriteRenderer;
+	internal void UpdateInner()
+	{
+	}
+
+    public void setAngle(float fAng)
+    {
+		if (fAng >= -90.0f && fAng <= 90.0f )
+		{
+			this.spriteRenderer.flipX = true;
+		}
+		else
+		{
+			this.spriteRenderer.flipX = false;
+		}
+	}
+
+    public void SetDirection(Vector2 dir)
+    {
+        this.transform.position += new Vector3( dir.x, dir.y) * fCoefMove;
+
+    }
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();   
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
