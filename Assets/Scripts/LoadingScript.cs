160b65c36df62665ff4418c95775641ce493d105
anton.hritsan@gameloft.com <anton.hritsan@gameloft.com>
2022-11-13 09:06:25 +0200

    - added animation for loading screen + added ode for playing anims

--- Assets/Scripts/LoadingScript.cs
+++ Assets/Scripts/LoadingScript.cs
@@ -10,12 +10,19 @@ public class LoadingScript : MonoBehaviour
 	public float fTimeToShowPart1 = 5.0f;
 	public float fTimeToShowPart2 = 15.0f;
 	public float fTimeToShowPart3 = 25.0f;
+
+	public GameObject ObjPart1;
+	public GameObject ObjPart2;
+	public GameObject ObjPart3;
 	// Start is called before the first frame update
 
 	void Start()
     {
-        
-    }
+		this.ObjPart1.GetComponent<Renderer>().enabled = false;
+		this.ObjPart2.GetComponent<Renderer>().enabled = false;
+		this.ObjPart3.GetComponent<Renderer>().enabled = false;
+
+	}
 
     // Update is called once per frame
     void Update()
@@ -30,23 +37,30 @@ public class LoadingScript : MonoBehaviour
 		{
 			// TO DO ONE
 			fTimeToShowPart1 = 9999999.0f;
+			this.ObjPart1.GetComponent<Animator>().Play("part1", -1);
+			this.ObjPart1.GetComponent<Renderer>().enabled = true;
 		}
 
 		if (fTimeToShowPart2 <= 0)
 		{
 			// TO DO TWO
 			fTimeToShowPart2 = 9999999.0f;
+			this.ObjPart2.GetComponent<Animator>().Play("part2", -1);
+			this.ObjPart2.GetComponent<Renderer>().enabled = true;
 		}
 
 		if (fTimeToShowPart3 <= 0)
 		{
 			// TO DO THREE
 			fTimeToShowPart3 = 9999999.0f;
+			this.ObjPart3.GetComponent<Animator>().Play("part3", -1);
+			this.ObjPart3.GetComponent<Renderer>().enabled = true;
 		}
 
 		if (fTimeToShowLoading <= 0)
 		{
 			SceneManager.LoadScene("SampleScene");
+
 		}
 
 	}
